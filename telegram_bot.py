4c14d14bf4176bf731a94adecd1978528974612f
Olena <72365541+olensa@users.noreply.github.com>
2021-04-06 11:29:59 -0400

    Update telegram_bot.py

--- telegram_bot.py
+++ telegram_bot.py
@@ -1,6 +1,6 @@
 import telebot
 
-bot = telebot.TeleBot('1722790569:AAHh_sP43w8VGdclJUIbwvC3YnE5RWROPDc')
+bot = telebot.TeleBot('TOKEN')
 @bot.message_handler(commands=['start', 'help'])
 def send_welcome(message):
     bot.reply_to(message, f'I am Bot. Noce to meet you, {message.from_user.first_name}')
